20e5ae01e7d6c3a993237711f7fd9def62fe8a32
kohei <kohei@koheinoAir.lan>
2017-03-30 19:30:55 +0900

    koharu

--- db/schema.rb
+++ db/schema.rb
@@ -37,9 +37,6 @@ ActiveRecord::Schema.define(version: 20170330100054) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "uid",                    default: "", null: false
-    t.string   "provider",               default: "", null: false
-    t.string   "image_url"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
@@ -50,6 +47,5 @@ ActiveRecord::Schema.define(version: 20170330100054) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
 
 end
